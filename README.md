74d6ad74375dffb0b2c44ae1fde0934812e08854
Bethany Colon <bethany.colon28@gmail.com>
2024-01-29 16:17:51 -0500

    fixed bugs and finalized project

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # oop-svg-logo-maker
+
+## Badges
+
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+  ## Table of Contents
+
+  * [License](#license)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [Tests](#tests)
+  * [Questions?](#questions)
+
+
+  ## License
+
+  This project was made using the MIT license.
+  [License Link](https://opensource.org/licenses/MIT)
+
+
+  ## Description
+
+  This project was created to create simple logos using inquirer, node js, and jest. With this tool, I can easily create a .svg file that creates a shape with text of the user's choice.
+
+
+  ## Installation
+
+  Clone this repository and open in VS Code. Then, install Node.JS Version 16.18.1, Jest Version 29.4.3, and Inquirer.js: Version 8.2.4.
+
+
+  ## Usage
+
+  To use this app, the user must go into the terminal and install inquirer fs and jest. Once that is done, they will enter "node index.js" to answer the prompted questions. Once that is complete, they will have their customized logo in a new file, generated-logo.svg.
+  
+  ![GIF of application walkthrough](/examples/SVG-Logo-Generator-Walkthrough.gif)
+
+
+  ## Credits
+  
+  Github: N/A
+
+
+  ## Tests
+
+  I tested this project by running and previewing it, as I have put above. I also created a test file specifically for that use, so that when I run npm test, jest runs a series of tests that either pass or fail.
+
+
+  ## Questions?
+  ### Reach Me Here: 
+
+  [bcolo24](https://github.com/bcolo24)
--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Square, Triangle } = require('./shapes'); // Import shapes module
+const { Circle, Square, Triangle } = require('./lib/shapes');
+
+// Function to validate and format colors
+function formatColor(input) {
+    return input; 
+}
+
+// Function to generate SVG content based on user input
+function generateSVG(shape, text, text_color, shape_color) {
+    let svgContent = '';
+
+    switch (shape) {
+        case 'Circle':
+            svgContent = new Circle(text, formatColor(text_color), formatColor(shape_color)).svgContent;
+            break;
+        case 'Square':
+            svgContent = new Square(text, formatColor(text_color), formatColor(shape_color)).svgContent;
+            break;
+        case 'Triangle':
+            svgContent = new Triangle(text, formatColor(text_color), formatColor(shape_color)).svgContent;
+            break;
+        default:
+            console.error('Invalid shape.');
+    }
+
+    return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+            <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                ${svgContent}
+            </svg>`;
+}
 
 // Prompt the user for input using Inquirer
 inquirer
@@ -9,23 +38,27 @@ inquirer
             type: 'list',
             name: 'shape',
             message: 'Select a shape:',
-            choices: ['circle', 'square', 'triangle'],
+            choices: ['Circle', 'Square', 'Triangle'],
         },
         {
             type: 'input',
-            name: 'characters',
+            name: 'text',
             message: 'Enter three characters:',
             validate: (input) => input.length === 3 || 'Please enter exactly three characters.',
         },
         {
-            type: 'list',
-            name: 'color',
-            message: 'Select a color:',
-            choices: ['red', 'green', 'blue', 'yellow', 'purple'],
+            type: 'input',
+            name: 'text_color',
+            message: 'Enter a color or hexadecimal number for the text:'
+        },
+        {
+            type: 'input',
+            name: 'shape_color',
+            message: 'Enter a color or hexadecimal number for the shape:'
         },
     ])
     .then((answers) => {
-        const svgContent = generateSVG(answers.shape, answers.characters, answers.color);
+        const svgContent = generateSVG(answers.shape, answers.text, answers.text_color, answers.shape_color);
 
         // Save the generated SVG content to a file
         fs.writeFileSync('generated-logo.svg', svgContent);
